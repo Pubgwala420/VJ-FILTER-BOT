9b965609001d9c02e771be427cc04f86cf368ba4
Movies8777 <roxedits07@gmail.com>
2025-01-29 14:02:48 +0530

    Update info.py

--- info.py
+++ info.py
@@ -17,7 +17,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 
 # This Pictures Is For Start Message Picture, You Can Add Multiple By Giving One Space Between Each.
-PICS = (environ.get('PICS', 'https://i.ibb.co/RDV7x8h/EOYWYOUIGRENNWG.jpg')).split()
+PICS = (environ.get('PICS', 'https://i.ibb.co/BYWJLJb/BUCVSDFHFCFPXTG.jpg https://i.ibb.co/ZP7Qf57/JMOMGLIXWDCRSMP.jpg')).split()
 
 
 # Admins & Users
