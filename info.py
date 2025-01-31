9574a01445ff30fc4b81f2141162de5aea4e4118
Movies8777 <roxedits07@gmail.com>
2025-01-31 08:28:48 +0530

    Update info.py

--- info.py
+++ info.py
@@ -88,7 +88,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/Death_Movies')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+CkijKzh7u6dhMDA1')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/death_movie')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'Death_Movies') # Support Chat Link Without https:// or @
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/MSTERRRRRR')
